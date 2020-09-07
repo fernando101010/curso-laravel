c327667c63a083baef3ebd4521c9509e475c9143
fernando101010 <fernandovieira_si@hotmail.com>
2020-09-07 14:06:02 -0300

    aula 10

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Any permite todo tipo de acesso de verbo http. funciona com get, post, put...
+Route::any('/any', function(){
+    return 'Any';
+});
+
+//match permite todo tipo de verbo http, sendo especificado antes.
+Route::match(['get', 'post'], '/match', function(){
+    return 'match';
+});
+
+Route::get('/empresa', function(){
+    return 'Historia da Enmpresa';
+});
+
+Route::get('/contato', function(){
+    return view ('site.contact');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
